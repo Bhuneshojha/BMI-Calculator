8fa7247930ad985bfccc1a8cee27d81fea537ac5
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2024-03-31 13:27:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+//   inquirer BMI Calculator using typescript
+import inquirer from "inquirer";
+const questions = [
+    { type: 'number', name: 'weight', mesaage: 'wight(kg):' },
+    { type: 'number', name: 'height', mesaage: 'height(m):' }
+];
+const calculateBMI = (weight, height) => weight / (height * height);
+const main = async () => {
+    const { weight, height } = await inquirer.prompt(questions);
+    console.log(`BMI: ${calculateBMI(weight, height).toFixed(3)}`);
+};
+main();
